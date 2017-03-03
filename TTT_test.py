ca5eacdfad58a4e70a51a01ef29b7112658f0972
Quant <s4549121@bns.bns>
2017-03-03 10:50:09 -0500

    .

--- TTT_test.py
+++ TTT_test.py
@@ -5,6 +5,31 @@ import os.path
 from keras.models import model_from_json
 from TTT import TTT
 
+def print_board(tuz):
+	print("{:02d}|{:02d}|{:02d}".format(int(tuz[0]),int(tuz[1]),int(tuz[2])))
+	print("{:02d}|{:02d}|{:02d}".format(int(tuz[3]),int(tuz[4]),int(tuz[5])))
+	print("{:02d}|{:02d}|{:02d}".format(int(tuz[6]),int(tuz[7]),int(tuz[8])))
+	print("")
+
+def my_action():
+	return raw_input("Please enter your move: ")
+
+def model_action(state):
+	q = model.predict(state)[0]
+	ql = list()
+	for i in range(0,len(q)):
+		ql.append([i,q[i]])
+	ql = sorted(ql,reverse=True,key=lambda tuz: tuz[1])
+	tmp_idx = 0;
+	valid_action = False
+	while not valid_action:
+		action = ql[tmp_idx][0]
+		if state[0][action] == -1:
+			valid_action = True
+		else:
+			tmp_idx = tmp_idx + 1
+	return action
+	
 if __name__ == "__main__":
 	epsilon = 0.1
 	num_actions = 9
@@ -26,37 +51,18 @@ if __name__ == "__main__":
 	
 	while -1 in state:
 	
-		#next model action
-		q = model.predict(state)[0]
-		ql = list()
-		for i in range(0,len(q)):
-			ql.append([i,q[i]])
-		ql = sorted(ql,reverse=True,key=lambda tuz: tuz[1])
-		tmp_idx = 0;
-		valid_action = False
-		while not valid_action:
-			action = ql[tmp_idx][0]
-			if state[0][action] == -1:
-				valid_action = True
-			else:
-				tmp_idx = tmp_idx + 1
-				
-		state[0][action] = 1
-		
-		#print board
-		tuz = state[0]
-		print("{:02d}|{:02d}|{:02d}".format(int(tuz[0]),int(tuz[1]),int(tuz[2])))
-		print("{:02d}|{:02d}|{:02d}".format(int(tuz[3]),int(tuz[4]),int(tuz[5])))
-		print("{:02d}|{:02d}|{:02d}".format(int(tuz[6]),int(tuz[7]),int(tuz[8])))
-		print("")
-		
-		#ask user for action
-		my_action = raw_input("Please enter your move: ")
-		state[0][my_action] = 0
-		
-		#print board
-		tuz = state[0]
-		print("{:02d}|{:02d}|{:02d}".format(int(tuz[0]),int(tuz[1]),int(tuz[2])))
-		print("{:02d}|{:02d}|{:02d}".format(int(tuz[3]),int(tuz[4]),int(tuz[5])))
-		print("{:02d}|{:02d}|{:02d}".format(int(tuz[6]),int(tuz[7]),int(tuz[8])))
-		print("")
+		if True:
+			#model goes first
+			state[0][model_action(state)] = 1
+			print_board(state[0])
+			
+			state[0][my_action()] = 0
+			print_board(state[0])
+		else:
+			#player goes first
+			state[0][my_action()] = 0
+			print_board(state[0])
+			
+			state[0][model_action()] = 1
+			print_board(state[0])
+			
